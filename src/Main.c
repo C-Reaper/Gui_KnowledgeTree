b98db380f9e1db85c005b9d4f8d11de2d934945b
codeleaded <gnu.c.reaper@gmail.com>
2026-04-12 18:51:00 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,23 +1,28 @@
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/KnowledgeTree.h"
 
-
-
+KnowledgeTree kt;
 
 void Setup(AlxWindow* w){
+	kt = KnowledgeTree_New();
+    KnowledgeTree_Load(&kt,"./data/Root.yaml");
 	
+	Hashmap_Print(&kt.yamls);
 }
 void Update(AlxWindow* w){
-	
+	KnowledgeTree_Update(&kt,w->Strokes,GetMouse());
+
 	Clear(BLACK);
 
-	
+	KnowledgeTree_Render(&kt,WINDOW_STD_ARGS);
 }
 void Delete(AlxWindow* w){
-	
+	KnowledgeTree_Save(&kt);
+	KnowledgeTree_Free(&kt);
 }
 
 int main(){
-    if(Create("Knowledge Tree",2500,1200,1,1,Setup,Update,Delete))
+    if(Create("Knowledge Tree",1920,1080,1,1,Setup,Update,Delete))
         Start();
     return 0;
 }
